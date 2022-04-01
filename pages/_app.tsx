68f8a4646307415ab767a5e90ea9feb40be89632
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2022-04-01 13:18:46 +0530

    removing warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChaindId={ChainId.Rinkeby}>
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   )
